422a1a1c7bea1077e8ed70328eab8f06c63b5c36
JoaoPMarto <joaopomarto@tecnico.ulisboa.pt>
2023-04-13 23:41:56 +0100

    change a few things on the way we calculate labels

--- Functions/detector_func.m
+++ Functions/detector_func.m
@@ -9,22 +9,10 @@ function detector_func(minArea,maxArea,minAspect,maxAspect,nFrame,nFramesToKeep,
     str = ['%s%.' num2str(frameIdComp) 'd.%s'];
     vid4D = zeros([576 768 3 nFrame],'uint8'); % Specify data type
     
-    
-    
-    % Initialize parameters for pedestrian detection
-    %minArea = 300;
-    %maxArea = 5000;
-    %minAspect = 0.1;
-    %maxAspect = 1.0;
-    
     % Initialize map to store pedestrian labels
     pedLabels = containers.Map('KeyType', 'double', 'ValueType', 'any');
 
     prevPositions = [];  % empty matrix for previous positions of the pedestrians
-    %nFramesToKeep = 200;   % Set the number of frames to keep each pedestrian position on screen
-
-
-
     for k = 1 : nFrame
         % print the iteration number
         fprintf('Iteration %d\n', k);
@@ -49,6 +37,7 @@ function detector_func(minArea,maxArea,minAspect,maxAspect,nFrame,nFramesToKeep,
 
         % Initialize counter variable for pedestrian IDs
         pedCounter = 1;
+        pedID = 0;
 
         % Filter out small and non-rectangular components
         for i = 1:numLabels
@@ -56,9 +45,6 @@ function detector_func(minArea,maxArea,minAspect,maxAspect,nFrame,nFramesToKeep,
             area = props(i).Area;
             bbox = props(i).BoundingBox;
             aspectRatio = bbox(3) / bbox(4);
-    
-            % Initialize distances variable
-            distances = [];
 
             % Check if component meets size and shape requirements
             if area >= minArea && area <= maxArea && ...
@@ -67,43 +53,34 @@ function detector_func(minArea,maxArea,minAspect,maxAspect,nFrame,nFramesToKeep,
                 % Initialize Kalman filter for this object
                 z = [bbox(1)+bbox(3)/2; bbox(2)+bbox(4)/2]; % observation
                 x(1:2) = z;    % initialize state estimate with observation
-                
-                minDistance = [];
-                distanceThreshold = 20; % Set a threshold for the minimum distance between pedestrian positions to consider them as the same pedestrian
-                if ~isempty(prevPositions)
-                    distances = sqrt(sum((prevPositions(:,1:2) - repmat(z', size(prevPositions,1), 1)).^2, 2));
-                    [minDist, minIndex] = min(distances);
-                    
-                    % Check if the closest previous position is within a certain distance
-                    if minDist < distanceThreshold
-                        % Assign the ID of the closest pedestrian
-                        id = prevPositions(minIndex,3);
+
+                % Search for existing pedestrian labels whose position is close to the current component
+                bestDist = Inf;
+                bestLabel = {};
+                for id = keys(pedLabels)
+                    pos = pedLabels(id{:});
+                    dist = norm(x(1:2) - pos);
+                    if dist < bestDist && dist < 50  % use a threshold to determine whether the position is close enough
+                        bestDist = dist;
+                        bestLabel = id;
                     end
                 end
 
-                if ~isempty(minDistance) && minDistance <= distanceThreshold
-                    % Detection belongs to an existing pedestrian
-                    [~, idx] = min(distances);
-                    id = prevPositions(idx, 3);  % Assign the same ID as the corresponding pedestrian
+                % If an existing label is found, use it; otherwise, assign a new ID
+                if ~isempty(bestLabel) && length(bestLabel) == 1 && bestLabel{1} ~= 0
+                    pedID = bestLabel{1};
                 else
-                    % Detection belongs to a new pedestrian
-                    if isempty(pedLabels)
-                        id = 1;
-                    else
-                        id = max(cell2mat(pedLabels.values)) + 1;
+                    prevPositions = [prevPositions; x(1:2), pedCounter];  % Assign that empty matrix for previous pedestrian positions
+
+                    % Remove old positions that are no longer visible
+                    if size(prevPositions, 1) > nFramesToKeep
+                        prevPositions(1,:) = [];
                     end
+        
+                    % Assign a unique ID to this pedestrian
+                    pedID = pedCounter;
+                    pedCounter = pedCounter + 1;
                 end
-
-                prevPositions = [prevPositions; x(1:2) id];  % Assign that empty matrix for previous pedestrian positions
-
-                % Remove old positions that are no longer visible
-                if size(prevPositions, 1) > nFramesToKeep
-                    prevPositions(1,:) = [];
-                end
-    
-                % Assign a unique ID to this pedestrian
-                pedID = pedCounter;
-                pedCounter = pedCounter + 1;
 
                 % Draw bounding box and label on the current frame
                 label = sprintf('Pedestrian %d', pedID);
@@ -111,7 +88,7 @@ function detector_func(minArea,maxArea,minAspect,maxAspect,nFrame,nFramesToKeep,
                 
                 % Plot pedestrian position as black dots
                 
-                pedLabels(id) = i; % Update the dictionary with the new pedestrian id
+                pedLabels(pedID) = i; % Update the dictionary with the new pedestrian id
                 frame = insertShape(frame, 'FilledCircle', cat(2, prevPositions(:,1), prevPositions(:,2), ones(size(prevPositions,1),1)*2), 'Color','black', 'LineWidth', 1);
                 
             end
